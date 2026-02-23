07c0e2bc3bef3611c2cd5f55380d97f06ebaa973
Aryan Kuril <aryan@bombayblokes.com>
2026-02-23 23:46:00 +0530

    NAVBAR TESTING

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,13 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <Hero />
+        {/* <Hero />
         <Grid />
         <TwoExperience/>
         <RecentProjects />
         <Clients />
         <Experience />
-        <Approach />
+        <Approach /> */}
         <Footer />
       </div>
     </main>
