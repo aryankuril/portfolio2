4e3145b2c6b154a9ebcf2e42fb1df81cf6680d3f
Aryan Kuril <aryan@bombayblokes.com>
2026-02-23 23:15:32 +0530

    SCRET ERROR SLOVED

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,6 @@
-import { withSentryConfig } from "@sentry/nextjs";
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
 };
 
-const sentryWebpackPluginOptions = {
-    silent: true,
-    org: "javascript-mastery",
-    project: "javascript-nextjs",
-};
-
-const sentryOptions = {
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-    // tunnelRoute: "/monitoring",
-};
-
-export default withSentryConfig(
-    nextConfig,
-    sentryWebpackPluginOptions,
-    sentryOptions
-);
+export default nextConfig;
